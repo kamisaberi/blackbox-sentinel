3a76c110de9a7d019ac87da6a24b385d51ce1f16
kamran saberifard <kamisaberi@gmail.com>
2026-09-14 18:08:45 +0330

    some codes changed

---
+++ include/sentinel/sentinel.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// Core Framework Contracts
+#include "sentinel/module_interface.hpp"
+#include "sentinel/event_bus.hpp"
+#include "sentinel/config_manager.hpp"
+
+// Dynamic Commercial Plugin Contracts
+#include "sentinel/plugin.hpp"
+#include "sentinel/plugin_manager.hpp"
